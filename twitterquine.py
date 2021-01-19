502f8e91e18b4f3edf5061745440e947b0ca2e5c
Sam Butler <contact@sjtbutler.co.uk>
2021-01-19 20:52:01 +0000

    added comments/hashtags

--- twitterquine.py
+++ twitterquine.py
@@ -3,3 +3,4 @@ from conf import *
 a=t.Api(consumer_key=ck,consumer_secret=cs,access_token_key=atk,access_token_secret=ats)
 with open(__file__) as f:
     a.PostUpdate(f.read())
+#quine #twitterbot #python
